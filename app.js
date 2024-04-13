570467d62062f9c45d4e027cb13776dc3b53553e
Zarina171717 Shixaliyeva <sixeliyeva05@gmail.com>
2024-04-13 13:14:46 +0000

    url

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 "use strict";
 $(document).ready(function () {
-
+    $(document).on('keypress', function (e) {
+        if (e.which == 13) {
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("myWeather").val()}&appid=bc2c00e8e5f27b506fbea3429ba7a3b2`
+        }
+    });
 });
