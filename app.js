5ef70b49457c37f73f7264769740c48c1fa77cf2
Zarina171717 Shixaliyeva <sixeliyeva05@gmail.com>
2024-10-13 13:51:09 +0000

    box

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 "use strict";
 $(document).ready(function () {
 
+
     $(document).on('keypress', function (e) {
         if (e.which == 13) {
             let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=bc2c00e8e5f27b506fbea3429ba7a3b2&units=metric`;
@@ -15,9 +16,10 @@ $(document).ready(function () {
             }
             myweather()
         }
+
     });
 
-    $(".btn").click(function (e) {
+    $("#btn").click(function (e) {
         e.preventDefault();
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=bc2c00e8e5f27b506fbea3429ba7a3b2&units=metric`;
         async function myweather() {
